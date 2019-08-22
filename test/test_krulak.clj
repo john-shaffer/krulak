ac05fb349c77099487313b031514875fd5e4c2a0
John Shaffer <jshaffer2112@gmail.com>
2019-08-22 16:25:56 -0500

    Add tests for base64url-uuid.

--- test/test_krulak.clj
+++ test/test_krulak.clj
@@ -7,3 +7,8 @@
          (deep-merge {:a 1 :b {:x 0 :y 3}} {:b {:x 1} :c 2})))
   (is (= {:a {:a 1} :b {:b 2}}
          (deep-merge {:a nil :b {:b 2}} {:a {:a 1} :b nil}))))
+
+
+(deftest test-base64url-uuid
+  (is (= 21 (count (base64url-uuid))))
+  (is (= 32 (count (base64url-uuid 32)))))
