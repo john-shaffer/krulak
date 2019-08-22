317da504d129a27b2f969f89ab63183c86bf4271
John Shaffer <jshaffer2112@gmail.com>
2019-08-22 12:11:40 -0500

    Add uuid, rand-bytes, base64url-encode, and base64url-uuid functions.

--- src/krulak.clj
+++ src/krulak.clj
@@ -11,3 +11,29 @@
        (fn [[new-name old-name]]
          `(defalias ~new-name ~old-name))
        (partition 2 syms))))
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn rand-bytes [n]
+  {:pre [(number? n)]}
+  (let [arr (byte-array n)]
+    (.nextBytes
+      (java.security.SecureRandom/getInstance "SHA1PRNG")
+      arr)
+    arr))
+
+(defn base64url-encode
+  "Return a base64url encoded string as defined by RFC 4648 §5 at
+  https://tools.ietf.org/html/rfc4648#section-5"
+  [byte-arr]
+  (.encodeToString (java.util.Base64/getUrlEncoder) byte-arr))
+
+(defn base64url-uuid
+  "Return a random URL-safe base64 UUID. The default length is 21 characters,
+  which represent 126 bits, 2 less bits than a normal UUID."
+  ([] (base64url-uuid 21))
+  ([length]
+     (->> (rand-bytes length)
+          base64url-encode
+          (take length)
+          (apply str))))
