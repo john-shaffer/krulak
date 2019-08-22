5062be6750265a600bddacfe9a36337ddcebea8e
John Shaffer <jshaffer2112@gmail.com>
2019-08-22 12:10:27 -0500

    Add defalias and defaliases macros.

--- src/krulak.clj
+++ src/krulak.clj
@@ -1,2 +1,13 @@
 (ns krulak
   "krulak is a web utility library for Clojure.")
+
+(defmacro defalias [new-name old-name]
+  `(let [doc-str# (:doc (meta #'~old-name))]
+     (def ^{:doc doc-str#} ~new-name @#'~old-name)))
+
+(defmacro defaliases [& syms]
+  `(do
+    ~@(map
+       (fn [[new-name old-name]]
+         `(defalias ~new-name ~old-name))
+       (partition 2 syms))))
