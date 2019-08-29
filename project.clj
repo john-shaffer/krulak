dce4ac9614526a6eeb46a7ad1975c7df4c8bcd65
John Shaffer <jshaffer2112@gmail.com>
2019-08-29 17:17:04 -0500

    Use clojars as default deploy option.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :deploy-repositories [["releases" :clojars]]
+  :deploy-repositories [["releases" :clojars
+                         "snapshots" :clojars]]
   :repl-options {:init-ns krulak})
