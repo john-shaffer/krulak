581b0edf60d31109e3a535172a218bb573f00db5
John Shaffer <jshaffer2112@protonmail.com>
2020-12-16 14:45:39 -0600

    Add krulak.http with some http and json convenience fns.

---
+++ src/krulak/http.clj
@@ -0,0 +1,43 @@
+(ns krulak.http
+  (:require [cheshire.core :as json]
+            [clj-http-failjure.client :as http]
+            [failjure.core :as f]
+            [krulak :as kr])
+  (:refer-clojure :exclude [get]))
+
+(kr/defaliases
+  get http/get
+  head http/head
+  post http/post
+  put http/put
+  delete http/delete
+  options http/options
+  copy http/copy
+  move http/move
+  patch http/patch)
+
+(defn json-get [url & [req]]
+  (let [req (assoc req :accept :json)]
+    (f/when-let-ok? [resp (get url req)]
+      (if (empty? (:body resp))
+        (assoc resp :body nil)
+        (f/try-all [json-resp (json/parse-string (:body resp))]
+          (assoc resp :body json-resp))))))
+
+(defn json-request [http-method-f url req body]
+  (let [req (assoc req :accept :json :content-type :json)]
+    (f/try-all [json (json/generate-string body)]
+      (f/when-let-ok? [resp (http-method-f url (assoc req :body json))]
+        (if (empty? (:body resp))
+          (assoc resp :body nil)
+          (f/try-all [json-resp (json/parse-string (:body resp))]
+            (assoc resp :body json-resp)))))))
+
+(defn json-post [url req body]
+  (json-request post url req body))
+
+(defn json-put [url req body]
+  (json-request put url req body))
+
+(defn json-delete [url req body]
+  (json-request delete url req body))
